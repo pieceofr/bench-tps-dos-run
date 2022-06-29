04ca835144da9d9fe66144f672619a2632c00954
pieceofr <komimi.p@gmail.com>
2022-06-29 19:32:58 +0800

    change image and service acct

--- create_gce.sh
+++ create_gce.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-vm_name=bench-tps-dos
-project=tour-de-sol
-img_name=bench-tps-dos-220628
+vm_name=dos-test-1
+project=principal-lane-200702
+img_name=dos-test-220629
 zone=asia-east1-b
 machine_type=n1-standard-32
 network_tag=http-server,https-server
@@ -13,7 +13,7 @@ gcloud beta compute instances create $vm_name \
 	--machine-type=$machine_type \
 	--network-interface=network-tier=PREMIUM,subnet=default \
 	--maintenance-policy=MIGRATE \
-	--service-account=bench-tps-dos-testing@tour-de-sol.iam.gserviceaccount.com \
+	--service-account=dos-test@principal-lane-200702.iam.gserviceaccount.com \
 	--scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
 	--tags=$network_tag \
 	--create-disk=auto-delete=yes,boot=yes,device-name=validator-template-for-eng,image=projects/ubuntu-os-pro-cloud/global/images/ubuntu-pro-2004-focal-v20211216,mode=rw,size=1024,type=projects/tour-de-sol/zones/asia-east1-b/diskTypes/pd-ssd \
