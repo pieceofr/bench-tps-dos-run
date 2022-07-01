e2c6f8d6b4e653656f9c6509d3d73dc66928ccaf
pieceofr <komimi.p@gmail.com>
2022-07-01 16:47:01 +0800

    fix wrong code

--- simple_test.sh
+++ simple_test.sh
@@ -4,4 +4,4 @@ set -ex
 
 gsutil cp  gs://bench-tps-dos/id_ed25519_dos_test ./
 chmod 600 id_ed25519_dos_test
-ssh -i id_ed25519_dos_test -o StrictHostKeyChecking=no sol@34.81.59.225 'bash -s' < exec-pre-start.sh
+ssh -i id_ed25519_dos_test -o StrictHostKeyChecking=no sol@34.81.59.225
