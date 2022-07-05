7cfa701551b36e153e6e9f9a73c2ce91a4385c75
pieceofr <komimi.p@gmail.com>
2022-07-05 15:48:29 +0800

    change image

--- dos-run.sh
+++ dos-run.sh
@@ -48,7 +48,7 @@ download_file() {
 create_gce() {
 	vm_name=dos-test-`date +%y%m%d-%M-%S`
 	project=principal-lane-200702
-	img_name=dost-test-220704-tpu-quic-udp
+	img_name=dos-test-220705-no-agent-solana-prebuild-2
 	if [[ ! "$zone" ]];then
 		zone=asia-east1-b
 	fi
