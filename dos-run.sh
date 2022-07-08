96c0d03fe2928074b7af4ed4563ee3e35d166b51
pieceofr <komimi.p@gmail.com>
2022-07-08 17:08:58 +0800

    change firewall to test

--- dos-run.sh
+++ dos-run.sh
@@ -54,7 +54,7 @@ create_gce() {
 		zone=asia-east1-b
 	fi
 	machine_type=n1-standard-32
-	network_tag=http-server,https-server
+	network_tag=allow-everything
 	ret_create=$(gcloud beta compute instances create $vm_name \
 		--project=$project \
 		--source-machine-image=$img_name \
