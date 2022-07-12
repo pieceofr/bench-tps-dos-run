5f762c5195f00372117910821156a87e80645581
pieceofr <komimi.p@gmail.com>
2022-07-12 14:38:22 +0800

    modify time format

--- dos-report.sh
+++ dos-report.sh
@@ -336,8 +336,8 @@ printf -v test_config "%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n" \
 		"duration = $duration" "tx_count = $tx_count" "thread_batch_sleep_ms = $thread_batch_sleep_ms"
 		
 # Construct Slack Result_Details Report
-printf -v s_time_start "time_start:%s%s" "$(date -u -d @$start_time)" "\\n"
-printf -v s_time_end "time_end:%s%s" "$(date -u -d @$stop_time)" "\\n"
+printf -v s_time_start "%s%s" "time_start: $(date -u -d @$start_time)" "\\n"
+printf -v s_time_end "%s%s" "time_end: $(date -u -d @$stop_time)" "\\n"
 printf -v s_slot "%s%s%s%s" $start_slot_txt "\\n" $end_slot_txt "\\n"
 printf -v s_tx_count "%s%s%s%s%s%s%s%s%s%s" $mean_tx_count_txt "\\n" $max_tx_count_txt "\\n" $p90_tx_count_txt "\\n" $p99_tx_count_txt "\\n"
 printf -v s_tower_vote_distance "%s%s%s%s%s%s%s%s" $mean_tower_vote_distance_txt "\\n" $max_tower_vote_distance_txt "\\n" $p90_tower_vote_distance_txt "\\n" $p99_tower_vote_distance_txt "\\n"
